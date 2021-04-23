a7695e6994ea9f9c2cf9f8121b7f7c37c83a0653
Noah Lanson <lanson.noah@gmail.com>
2021-04-23 21:01:40 +1000

    following commit

--- GameEngineSrc/GameObject/Physics.ts
+++ GameEngineSrc/GameObject/Physics.ts
@@ -1,6 +1,8 @@
 import { Vector2 } from "../Typings/Interfaces";
 
 export abstract class Physics {
+    
+    //Theres probably a shit tonne more maths than this to add forces.
     calculateForce(position: Vector2, force: Vector2): Vector2 {
         position.x = position.x + force.x;
         position.y = position.y + force.y;
